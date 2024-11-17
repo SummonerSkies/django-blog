5e1db9bcf499cae46d14153316e7607abf4c40e1
Emma Sadler <summonerskies@gmail.com>
2024-11-17 22:02:17 +0000

    readying code for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-=b=)xb^gw64yw+x!3k(kj171u!t-n8+brf@*(xjq4s_zoeeu29
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-summonerskie-djangoblog-kjhlhkxy35w.ws.codeinstitute-ide.net',
-                 '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-summonerskie-djangoblog-kjhlhkxy35w.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
