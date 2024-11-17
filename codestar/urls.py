ee581679ece439bb502626e514a0c4dd72b386f5
Emma Sadler <summonerskies@gmail.com>
2024-11-17 21:44:07 +0000

    Corrected errors in codestar urls

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import my_blog
+from blog import views as my_blog
 
 urlpatterns = [
+    path("blog/", blog_views.my_blog, name='blog'),
     path('admin/', admin.site.urls),
-    path('blog/', my_blog, name='blog'),
 ]
