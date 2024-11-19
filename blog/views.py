1dbe6d289888d2fd4aaf9e3110159e41a3280599
Emma Sadler <summonerskies@gmail.com>
2024-11-19 15:23:14 +0000

    added blog templates and updating views and urls py files

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
-# Create your views here.
-def my_blog(request):
-    return HttpResponse("Hello, Blog!")
+    # Create your views here.abs
+
+
+    class PostList(generic.ListView):
+        model = Post
